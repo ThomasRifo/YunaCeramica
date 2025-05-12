00061120c7732ed61c549a3559f03cf9b49bda66
thomasrifo <thomasmrifo@gmail.com>
2025-05-12 12:52:02 -0300

    .

--- YunaCeramica/database/seeders/DatabaseSeeder.php
+++ YunaCeramica/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,7 +19,9 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'apellido' => 'Test Apellido',
             'email' => 'test@example.com',
+            'email_verified_at' => now(),
             'password' => bcrypt('password'),
+            'remember_token' => Str::random(10),
         ])->assignRole('admin');
     
         $this->call([
